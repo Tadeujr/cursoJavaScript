e6f3f61f65c50e887c76d04d53a576c30b7bc6b8
Tadeujr <tadeu_junior@outlook.com>
2019-07-25 10:37:18 -0300

    utilizando o nodemon

--- node/projeto/src/serv.js
+++ node/projeto/src/serv.js
@@ -1,10 +1,25 @@
-const porta =  3003 
-
+const porta =  3003
 const express =  require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados') 
 
-app.get('/produtos',(req,res,next)=>{
-    res.send({nome:'notebook',preco:123.45})
+app.use(bodyParser.urlencoded({extended: true})) // lembrar de baixar o modulo bodyParser
+
+app.get('/produtos',(req,res,next) => {
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id',(req,res,next)=>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos',(req,res,next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome:req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)//JSON
 })
 
 app.listen(porta,()=>{
